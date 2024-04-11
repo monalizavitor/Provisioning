cee46f080ff1978074266dd4362e1ffda72c7dbc
Monaliza Vitor <65304621+monalizavitor@users.noreply.github.com>
2024-04-10 23:12:39 -0300

    Revert "App python"

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -18,6 +18,6 @@ resource "aws_instance" "app_server" {
   instance_type = "t2.micro"
   key_name = "iac-key"
   tags = {
-    Name = "Terraform-Ansible-Python"
+    Name = "Primeira instancia"
   }
 }
